8031c2519752fcbb0c3434c915f60a248641d373
poojashinde2522001-droid <poojashinde2522001@gmail.com>
2026-03-12 21:01:24 +0530

    Expand student management SQL with courses and enrollments
    Added Courses and Enrollments tables with sample data.

--- student_management.sql
+++ student_management.sql
@@ -13,4 +13,34 @@ INSERT INTO Students VALUES
 (2,'Sneha',22,'Mumbai'),
 (3,'Amit',20,'Kolhapur');
 
+
 SELECT * FROM Students;
+
+CREATE TABLE Courses(
+CourseID INT PRIMARY KEY,
+CourseName VARCHAR(50)
+);
+
+CREATE TABLE Enrollments(
+EnrollID INT PRIMARY KEY,
+StudentID INT,
+CourseID INT,
+FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
+FOREIGN KEY (CourseID) REFERENCES Courses(CourseID)
+);
+
+INSERT INTO Courses VALUES
+(101,'SQL'),
+(102,'Python'),
+(103,'Power BI');
+
+INSERT INTO Enrollments VALUES
+(1,1,101),
+(2,2,102),
+(3,3,103);
+
+
+SELECT S.Name, C.CourseName
+FROM Students S
+JOIN Enrollments E ON S.StudentID = E.StudentID
+JOIN Courses C ON E.CourseID = C.CourseID;
